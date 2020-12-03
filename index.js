40dd126c2f3ef32b94cb20930dd9732635c44664
Vishwas Paikra <vishwaspaikra007@gmail.com>
2020-12-03 23:21:06 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express()
 const fb = require('./firebase')
 
 app.use(cors())
-app.options('*', cors());
+app.use(cors(
+    {origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3001','http://localhost:3000', 'https://vishwaspaikra007.github.io', 'https://fit-x-backend.herokuapp.com'],
+     credentials: true}
+     ))
+app.options('*', cors())  // enable pre-flight request for complex cors request for every route
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
